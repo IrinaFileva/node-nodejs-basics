d6c48e2d8592b45a61d1fa247ea1860c19e8c5ae
Irina Fileva <irinkaskai@gmail.com>
2024-10-01 17:23:40 +0500

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const content = 'I am fresh and young';
+const errorMessage = 'FS operation failed';
+const pathNewFile = path.join(__dirname, 'files', 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.promises.access(pathNewFile)
+        throw new Error(errorMessage)
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            await fs.promises.writeFile(pathNewFile, content);
+        } else {
+            console.error(err.message);
+        }
+    }
 };
 
 await create();
