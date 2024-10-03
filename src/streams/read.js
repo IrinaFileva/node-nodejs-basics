f7c6133d293d9e1cd9625c05349a01b2a6444df4
Irina Fileva <irinkaskai@gmail.com>
2024-10-03 15:30:11 +0500

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import { stdout } from 'process';
+import { createReadStream } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathFile = path.join(__dirname, 'files', 'fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+
+    const readText = createReadStream(pathFile);
+    readText.on('data',(details) => stdout.write(details + "\n"))
 };
 
 await read();
