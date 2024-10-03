4b8767c184c81202046cf87b12c8289f692a9a73
Irina Fileva <irinkaskai@gmail.com>
2024-10-03 15:11:16 +0500

    feat: implement calcHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const { createHash } = await import('crypto');
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+const hash = createHash('sha256');
+const input = fs.createReadStream(filePath);
+
 const calculateHash = async () => {
-    // Write your code here 
+   const data = input.pipe(hash).setEncoding('hex');
+   data.on('data', (details) => console.log(details));
 };
 
 await calculateHash();
