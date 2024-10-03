a6152f7c39efdbbe69c958916e2da350051135a1
Irina Fileva <irinkaskai@gmail.com>
2024-10-03 16:10:43 +0500

    feat: implement transform function

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import { stdin, stdout } from 'process';
+import { Transform } from 'stream';
+
 const transform = async () => {
-    // Write your code here 
+    const reverseTransform = new Transform({
+        transform(data, _, callback) {
+            const reverseData = data.toString().split('').reverse().join('') + '\n';
+            callback(null, reverseData);
+        }
+    });
+
+    stdin.pipe(reverseTransform);
+    reverseTransform.on('data',(details) => stdout.write(details + "\n"));
 };
 
 await transform();
